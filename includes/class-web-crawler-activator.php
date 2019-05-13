9a6f067721cd7bf9c0ac9d5ff0ac1aec3c5d2b4e
Huỳnh Ngọc Hoàng Phúc <huynhngochoangphuc@gmail.com>
2019-05-13 10:29:05 +0700

    Add migration

--- includes/class-web-crawler-activator.php
+++ includes/class-web-crawler-activator.php
@@ -21,7 +21,7 @@
  * @author     phuchnh <huynhngochoangphuc@gmail.com>
  */
 class Web_Crawler_Activator {
-
+	
 	/**
 	 * Short Description. (use period)
 	 *
@@ -30,7 +30,46 @@ class Web_Crawler_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+		static::create_table_crawl_domains();
+		static::create_table_crawl_categories();
 	}
-
+	
+	private static function create_table_crawl_categories() {
+		global $wpdb;
+		$charset_collate  = $wpdb->get_charset_collate();
+		$crawl_categories = $wpdb->prefix . 'crawl_categories';
+		$crawl_domains    = $wpdb->prefix . 'crawl_domains';
+		
+		$sql = "CREATE TABLE IF NOT EXISTS {$crawl_categories} (
+            id               int(11) UNSIGNED                   NOT NULL AUTO_INCREMENT,
+		    crawl_domain_id  int(11) UNSIGNED                   NOT NULL,
+		    category_url     text                               NOT NULL,
+		    category_options longtext,
+		    created_at       datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		    updated_at       datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+		    PRIMARY KEY (id),
+		    FOREIGN KEY (crawl_domain_id) REFERENCES {$crawl_domains} (id) ON DELETE CASCADE
+        ) {$charset_collate};";
+		
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta( $sql );
+	}
+	
+	private static function create_table_crawl_domains() {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$crawl_domains   = $wpdb->prefix . 'crawl_domains';
+		$sql             = "CREATE TABLE IF NOT EXISTS {$crawl_domains} (
+            id             int(11) UNSIGNED                   NOT NULL AUTO_INCREMENT,
+		    domain_url     text                               NOT NULL,
+		    domain_options longtext,
+		    created_at     datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		    updated_at     datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+		    PRIMARY KEY (id)
+        ) {$charset_collate};";
+		
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta( $sql );
+	}
+	
 }
