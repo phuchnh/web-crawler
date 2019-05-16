ae716a946eb942e1c420d63d7130134b35d2f026
Huỳnh Ngọc Hoàng Phúc <huynhngochoangphuc@gmail.com>
2019-05-16 13:49:07 +0700

    Update

--- app/Controllers/CrawlSettingController.php
+++ app/Controllers/CrawlSettingController.php
@@ -98,6 +98,24 @@ class CrawlSettingController extends Controller
      */
     public function update(CrawlSetting $crawl_setting)
     {
+    
+        $options = [
+            'crawl_domain_id' => 'required|unique:crawl_domain_id:wp_crawl_settings@id:'.$crawl_setting->id,
+        ];
+    
+        $validator = tr_validator($options, $this->request->getFields());
+    
+        $validator->setErrorMessages([
+            'crawl_domain_id:required' => 'Domain is require.',
+            'crawl_domain_id:unique'   => 'Domain is taken.',
+        ])->validate();
+    
+        if ($validator->getErrors()) {
+            $validator->flashErrors($this->response);
+        
+            return tr_redirect()->toPage($this->resource, 'edit', $crawl_setting->id)
+                                ->withFields($this->request->getFields());
+        }
         
         $crawl_setting->crawl_domain_id = $this->request->getFields('crawl_domain_id');
         $crawl_setting->categories      = $this->request->getFields('categories');
@@ -109,40 +127,22 @@ class CrawlSettingController extends Controller
     }
     
     /**
-     * The show page for admin
-     *
-     * @param $id
-     *
-     * @return mixed
-     */
-    public function show($id)
-    {
-        // TODO: Implement show() method.
-    }
-    
-    /**
-     * The delete page for admin
-     *
-     * @param $id
-     *
-     * @return mixed
-     */
-    public function delete($id)
-    {
-        // TODO: Implement delete() method.
-    }
-    
-    /**
      * Destroy item
      *
      * AJAX requests and normal requests can be made to this action
      *
-     * @param $id
+     * @param  \App\Models\CrawlSetting  $crawl_setting
      *
      * @return mixed
+     * @throws \Exception
      */
-    public function destroy($id)
+    public function destroy(CrawlSetting $crawl_setting)
     {
-        // TODO: Implement destroy() method.
+        if ( ! $success = (boolean) $crawl_setting->delete()) {
+            return $this->response->flashNext('Setting deleted failure!', 'error');
+        }
+        $this->response->flashNext('Setting deleted!');
+    
+        return tr_redirect()->toPage($this->resource, 'index');
     }
 }
--- resources/pages/crawl_setting/index.php
+++ resources/pages/crawl_setting/index.php
@@ -1,18 +1,14 @@
 <?php /** @noinspection PhpUnhandledExceptionInspection */
 
 $table = tr_tables();
-$table->setColumns('id', [
-    'id'    => [
-        'sort'    => true,
-        'label'   => 'ID',
-        'actions' => ['edit', 'delete'],
-    ],
+$table->setColumns('crawl_domain_id', [
     'crawl_domain_id' => [
         'sort'     => true,
         'label'    => 'Domain URL',
+        'actions' => ['edit', 'delete'],
         'callback' => function ($id, \App\Models\CrawlSetting $result) {
             if ($domain = $result->domain) {
-                $url = tr_redirect()->toPage('crawl_domain', 'edit', $id)->url;
+                $url = tr_redirect()->toPage('crawl_setting', 'edit', $result->id)->url;
 
                 return (new \TypeRocket\Html\Generator())->newLink($domain->domain_url, $url);
             }
