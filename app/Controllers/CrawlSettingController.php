3076ec4d296e801acacf6523c0be16f99c696e2c
Huỳnh Ngọc Hoàng Phúc <huynhngochoangphuc@gmail.com>
2019-05-16 12:10:01 +0700

    Update code

--- app/Controllers/CrawlSettingController.php
+++ app/Controllers/CrawlSettingController.php
@@ -7,11 +7,11 @@ use \TypeRocket\Controllers\Controller;
 
 class CrawlSettingController extends Controller
 {
-
+    
     protected $modelClass = CrawlSetting::class;
-
+    
     protected $resource = 'crawl_setting';
-
+    
     /**
      * The index page for admin
      *
@@ -21,7 +21,7 @@ class CrawlSettingController extends Controller
     {
         return tr_view('crawl_setting.index');
     }
-
+    
     /**
      * The add page for admin
      *
@@ -30,10 +30,10 @@ class CrawlSettingController extends Controller
     public function add()
     {
         $form = tr_form($this->resource, 'create');
-
+        
         return tr_view('crawl_setting.add', ['form' => $form]);
     }
-
+    
     /**
      * Create item
      *
@@ -44,16 +44,34 @@ class CrawlSettingController extends Controller
      */
     public function create()
     {
+        $options = [
+            'crawl_domain_id' => 'required|unique:crawl_domain_id:wp_crawl_settings',
+        ];
+        
+        $validator = tr_validator($options, $this->request->getFields());
+        
+        $validator->setErrorMessages([
+            'crawl_domain_id:required' => 'Domain is require.',
+            'crawl_domain_id:unique'   => 'Domain is taken.',
+        ])->validate();
+        
+        if ($validator->getErrors()) {
+            $validator->flashErrors($this->response);
+            
+            return tr_redirect()->toPage($this->resource, 'add')
+                                ->withFields($this->request->getFields());
+        }
+        
         $crawl_setting                  = new CrawlSetting();
         $crawl_setting->crawl_domain_id = $this->request->getFields('crawl_domain_id');
         $crawl_setting->categories      = $this->request->getFields('categories');
-        $crawl_setting->options         = $this->request->getFields('options');
+        // $crawl_setting->options         = $this->request->getFields('options');
         $crawl_setting->save();
         $this->response->flashNext('Success!');
-
+        
         return tr_redirect()->toPage($this->resource, 'index');
     }
-
+    
     /**
      * The edit page for admin
      *
@@ -63,23 +81,33 @@ class CrawlSettingController extends Controller
      */
     public function edit($id)
     {
-        // TODO: Implement edit() method.
+        $form = tr_form($this->resource, 'update', $id);
+        
+        return tr_view('crawl_setting.edit', ['form' => $form]);
     }
-
+    
     /**
      * Update item
      *
      * AJAX requests and normal requests can be made to this action
      *
-     * @param $id
+     * @param  \App\Models\CrawlSetting  $crawl_setting
      *
      * @return mixed
+     * @throws \Exception
      */
-    public function update($id)
+    public function update(CrawlSetting $crawl_setting)
     {
-        // TODO: Implement update() method.
+        
+        $crawl_setting->crawl_domain_id = $this->request->getFields('crawl_domain_id');
+        $crawl_setting->categories      = $this->request->getFields('categories');
+        // $crawl_setting->options         = $this->request->getFields('options');
+        $crawl_setting->save();
+        $this->response->flashNext('Success!');
+        
+        return tr_redirect()->toPage($this->resource, 'index');
     }
-
+    
     /**
      * The show page for admin
      *
@@ -91,7 +119,7 @@ class CrawlSettingController extends Controller
     {
         // TODO: Implement show() method.
     }
-
+    
     /**
      * The delete page for admin
      *
@@ -103,7 +131,7 @@ class CrawlSettingController extends Controller
     {
         // TODO: Implement delete() method.
     }
-
+    
     /**
      * Destroy item
      *
--- resources/pages/crawl_setting/add.php
+++ resources/pages/crawl_setting/add.php
@@ -2,15 +2,74 @@
 /**
  * @var $form \TypeRocket\Elements\Form
  */
+$form->useOld();
 
 $domains = $form->select('crawl_domain_id')->setLabel('Domain URL');
 $domains->setModelOptions(new \App\Models\CrawlDomain, 'domain_url', 'id');
+$domains->setAttribute('id', 'domain');
 $domains->setAttribute('class', 'select2');
 
+$select = $form->select('category_url');
+$select->setAttribute('class', 'select2');
+$select->setLabel('Category URL');
+
+$options = array_map(function (\App\Models\CrawlCategory $value) {
+    return [
+        'id'        => $value->id,
+        'text'      => $value->category_url,
+        'domain_id' => $value->crawl_domain_id,
+    ];
+}, (array)(new \App\Models\CrawlCategory)->findAll()->get() ?? []);
+
+$repeater = $form->repeater('categories');
+$repeater->setFields([$select]);
+$repeater->setLabel('Categories');
+
 // Render form
-$form->useOld();
 echo $form->open();
 echo $domains;
-echo $form->items('categories')->setLabel('Categories');
+echo $repeater;
 echo $form->submit('Add');
 echo $form->close();
+
+?>
+
+<script>
+  (function($) {
+    'use strict';
+    $(function() {
+      var categories = <?php echo json_encode($options);?>;
+      var $domain = $('#domain');
+
+      function getCategoriesBelongsToDomain(domainId, $dom) {
+        // Empty options
+        $dom.empty();
+
+        // Get categories belongs to domain
+        var options = _.reduce(categories, function(result, value) {
+          if (+value['domain_id'] === +domainId) {
+            result.push(new Option(value['text'], value['id']));
+          }
+          return result;
+        }, []);
+
+        // Append new options
+        $dom.append(options);
+      }
+
+      TypeRocket.repeaterCallbacks.push(function($template) {
+        var $select = $template.find('select').select2({ width: '100%' });
+
+        // Remove unused element after re-init select2
+        $select.last().next().next().remove();
+        
+        getCategoriesBelongsToDomain($domain.val(), $select);
+      });
+
+      $domain.change(function(e) {
+        e.preventDefault();
+        $('.tr-repeater-fields').empty();
+      });
+    });
+  })(jQuery);
+</script>
