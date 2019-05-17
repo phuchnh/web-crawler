b0f3ad6a5ac09dd1d45673a9925fc4617da0c25c
Huỳnh Ngọc Hoàng Phúc <huynhngochoangphuc@gmail.com>
2019-05-17 22:39:15 +0700

    optimize code

--- app/Commands/CrawlLink.php
+++ app/Commands/CrawlLink.php
@@ -13,43 +13,48 @@ class CrawlLink extends Command
      * @var string
      */
     protected $domain_url;
-    
+
+    /**
+     * @var string
+     */
+    protected $pagination;
+
     protected $command = [
         'app:craw-link',
         'Short description here',
         'Longer help text goes here.',
     ];
-    
+
     protected function config()
     {
         // If you want to accept arguments
         // $this->addArgument('arg', self::REQUIRED, 'Description');
     }
-    
+
     /**
      * @throws \Exception
      */
     public function exec()
     {
-        $settings          = $this->getCategorySettings();
-        $categoryIds       = array_pluck($settings, 'category_id');
-        $categoryUrls      = $this->getCategoryUrls($categoryIds);
-        $categorySelectors = $this->getCategorySelectors();
-        $categorySettings  = $this->mapSelectors($categoryUrls, $categorySelectors);
-        $categorySettings  = array_chunk($this->mapPagination($settings, $categorySettings), 50);
-        
-        foreach ($categorySettings as $setting) {
+        $settings      = $this->getCategorySettings();
+        $cateIds       = array_pluck($settings, 'category_id');
+        $cateUrls      = $this->getCategoryUrls($cateIds);
+        $cateSelectors = $this->getCategorySelectors();
+        $cateSettings  = $this->mapSelectors($cateUrls, $cateSelectors);
+        $cateSettings  = array_chunk($this->mapPagination($settings, $cateSettings), 50);
+
+        foreach ($cateSettings as $setting) {
             $this->crawl($setting);
         }
-        
+
         // When command executes
         echo 'Success';
     }
-    
+
     /**
      * Start crawl links
      *
-     * @param  array  $categories
+     * @param  array $categories
      */
     protected function crawl(array $categories)
     {
@@ -59,23 +64,23 @@ class CrawlLink extends Command
             $selector         = array_get($categories[$index], 'selector');
             $single_options   = array_get($categories[$index], 'single_options');
             $this->domain_url = array_get($categories[$index], 'domain_url');
-            
+
             try {
                 $html = phpQuery::newDocumentFileHTML($url);
             } catch (\Exception $exception) {
                 continue;
             }
-            
+
             if ( ! $html) {
                 continue;
             }
-            
+
             try {
                 $elements = pq($selector)->elements;
             } catch (\Exception $exception) {
                 continue;
             }
-            
+
             $links = [];
             if (count($elements) > 0) {
                 foreach ($elements as $element) {
@@ -84,20 +89,20 @@ class CrawlLink extends Command
                     $links[] = sprintf("('%s', '%s')", $link, esc_sql($single_options));
                 }
             }
-            
+
             $this->createMany($links);
             $this->deleteDuplicateRows();
-            
+
             // Next
             $index++;
         } while ($index < count($categories));
     }
-    
+
     /**
      * Create links by pagination
      *
-     * @param  array  $settings
-     * @param  array  $categories
+     * @param  array $settings
+     * @param  array $categories
      *
      * @return array
      */
@@ -106,51 +111,54 @@ class CrawlLink extends Command
         $result = [];
         $index  = 0;
         while ($index < count($categories)) {
-            $category = $categories[$index];
-            
-            $matched = array_search($category['category_id'], array_column($settings, 'category_id'), true);
-            
+            $cate = $categories[$index];
+
+            // Set default pagination if not setting
+            $pagination = empty($cate['pagination']) ? 'page' : $cate['pagination'];
+
+            $matched = array_search($cate['category_id'], array_column($settings, 'category_id'), true);
+
             if ($matched > -1) {
                 $limit = (int)array_get($settings[$matched], 'page', 1);
                 $links = [];
                 for ($i = 1; $i <= $limit; $i++) {
                     $value                 = $categories[$matched];
-                    $value['category_url'] = $value['category_url'].'?page='.$i;
+                    $value['category_url'] = $value['category_url'] . "?{$pagination}=" . $i;
                     $links[]               = $value;
                 }
-                
-                $result = array_merge($result, $links);
+                $result[] = $links;
             }
             $index++;
         }
-        
-        return $result;
+
+        return array_merge(...$result);
     }
-    
+
     /**
-     * @param  array  $categoryUrls
-     * @param  array  $categorySelectors
+     * @param  array $cateUrls
+     * @param  array $cateSelectors
      *
      * @return array
      */
-    protected function mapSelectors(array $categoryUrls, array $categorySelectors)
+    protected function mapSelectors(array $cateUrls, array $cateSelectors)
     {
         $result = [];
-        foreach ($categoryUrls as $key => $value) {
-            $haystack = array_column($categorySelectors, 'id');
+        foreach ($cateUrls as $key => $value) {
+            $haystack = array_column($cateSelectors, 'id');
             $needle   = $value['crawl_domain_id'];
             $matched  = array_search($needle, $haystack, true);
             if ($matched > -1) {
-                $value['selector']       = array_get($categorySelectors[$matched], 'archive_options.selector');
-                $value['single_options'] = array_get($categorySelectors[$matched], 'single_options');
-                $value['domain_url']     = array_get($categorySelectors[$matched], 'domain_url');
+                $value['selector']       = array_get($cateSelectors[$matched], 'archive_options.selector');
+                $value['pagination']     = array_get($cateSelectors[$matched], 'archive_options.pagination');
+                $value['single_options'] = array_get($cateSelectors[$matched], 'single_options');
+                $value['domain_url']     = array_get($cateSelectors[$matched], 'domain_url');
                 $result[]                = $value;
             }
         }
-        
+
         return $result;
     }
-    
+
     /**
      * @return array
      * @throws \Exception
@@ -162,7 +170,7 @@ class CrawlLink extends Command
             ->findAll()
             ->select('id', 'archive_options', 'single_options', 'domain_url')
             ->get();
-        
+
         foreach ((array)$domains as $domain) {
             $value['id']              = $domain->id;
             $value['domain_url']      = $domain->domain_url;
@@ -170,58 +178,58 @@ class CrawlLink extends Command
             $value['single_options']  = array_get($domain->getPropertiesUnaltered(), 'single_options');
             $result[]                 = $value;
         }
-        
+
         return $result;
     }
-    
+
     /**
-     * @param  array  $categoryIds
+     * @param  array $cateIds
      *
      * @return array
      * @throws \Exception
      */
-    protected function getCategoryUrls(array $categoryIds = [])
+    protected function getCategoryUrls(array $cateIds = [])
     {
         $result     = [];
         $categories = tr_get_model('CrawlCategory')
-            ->where('id', 'IN', $categoryIds)
+            ->where('id', 'IN', $cateIds)
             ->select('crawl_domain_id', 'category_url', 'id')
             ->get();
-        
-        foreach ($categories as $category) {
-            $value['crawl_domain_id'] = data_get($category, 'crawl_domain_id');
-            $value['category_url']    = data_get($category, 'category_url');
-            $value['category_id']     = data_get($category, 'id');
+
+        foreach ($categories as $cate) {
+            $value['crawl_domain_id'] = data_get($cate, 'crawl_domain_id');
+            $value['category_url']    = data_get($cate, 'category_url');
+            $value['category_id']     = data_get($cate, 'id');
             $result[]                 = $value;
         }
-        
+
         return $result;
     }
-    
+
     /**
      * Insert multiple row
      *
-     * @param  array  $links
+     * @param  array $links
      */
     protected function createMany(array $links)
     {
         global $wpdb;
-        $crawl_link_table = $wpdb->prefix.'crawl_links';
+        $crawl_link_table = $wpdb->prefix . 'crawl_links';
         if (count($links) > 0) {
-            $sql = "INSERT INTO {$crawl_link_table} (`link`, `options`) VALUES ".implode(',', $links).';';
+            $sql = "INSERT INTO {$crawl_link_table} (`link`, `options`) VALUES " . implode(',', $links) . ';';
             $wpdb->query($sql);
         }
     }
-    
+
     protected function deleteDuplicateRows()
     {
         global $wpdb;
-        $crawl_link_table = $wpdb->prefix.'crawl_links';
+        $crawl_link_table = $wpdb->prefix . 'crawl_links';
         $sql              = "DELETE t1 FROM {$crawl_link_table} t1 INNER JOIN {$crawl_link_table} t2 WHERE t1.id < t2.id AND t1.link = t2.link;";
         $wpdb->query($sql);
     }
-    
-    
+
+
     /**
      * @return array
      * @throws \Exception
@@ -231,17 +239,19 @@ class CrawlLink extends Command
         // Get all crawl settings
         $settings = tr_get_model('CrawlSetting');
         $settings = (array)$settings->findAll()->select('categories')->get();
-        $result   = [];
-        
-        // Map category_id and pagination
-        foreach ($settings as $setting) {
-            /**@var $setting \App\Models\CrawlSetting */
+
+        /**
+         * Map category_id and pagination
+         * @example ['category_id' => '5', 'page' => '1']
+         */
+        $result = [];
+        array_walk($settings, function ($setting) use (&$result) {
             $result = array_merge($result, $setting->categories);
-        }
-        
+        });
+
         return $result;
     }
-    
+
     /**
      * @param $url string
      *
@@ -252,10 +262,10 @@ class CrawlLink extends Command
         if (preg_match('/^(https?:\/\/).*$/i', $url) > 0) {
             return $url;
         }
-        
+
         $scheme = parse_url($this->domain_url, PHP_URL_SCHEME);
         $host   = parse_url($this->domain_url, PHP_URL_HOST);
-        
+
         return sprintf('%s://%s/%s', $scheme, $host, trim($url, '/'));
     }
 }
